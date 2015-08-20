ca2e06c35bf937b30e47448142ca6f17339a4874
Michael Mokrysz <hi@46b.it>
2015-08-20 14:30:06 +0100

    Restructured toc2json TOC api to reflect that orders of intertwined layers and groups is important. Also added openlayer rendering functionality for taesp_maps. Addresses some of #17.

--- toc2json/api.js
+++ toc2json/api.js
@@ -2,20 +2,31 @@
 
 var TOC = (function () {
   function TOC(a, name, b, gif) {
+    this.element_type = "TOC"
     this.a = a
     this.name = name
     this.b = b
     this.gif = gif
-    this.layers = []
-    this.groups = []
+    this.layersAndGroups = []
   }
   TOC.prototype.addLayer = function addLayer(layer) {
-    this.layers.push(layer)
+    this.layersAndGroups.push(layer)
     return layer
   }
   TOC.prototype.addGroup = function addGroup(group) {
-    this.groups.push(group)
+    this.layersAndGroups.push(group)
     return group
+  }
+  TOC.prototype.asOpenLayers = function asOpenLayers(source_constructor) {
+    var mapped = []
+    for (var i in this.layersAndGroups) {
+      var layerOrGroup = this.layersAndGroups[i]
+      if (typeof layerOrGroup !== "LAYER" || layerOrGroup.codeIsNumeric()) {
+        var olLayerOrGroup = layerOrGroup.asOpenLayers(source_constructor)
+        mapped.push(olLayerOrGroup)
+      }
+    }
+    return mapped
   }
   return TOC
 })()
@@ -23,30 +34,61 @@ exports.TOC = TOC
 
 var GROUP = (function () {
   function GROUP(name, b) {
+    this.element_type = "GROUP"
     this.name = name
     this.b = b
-    this.layers = []
-    this.groups = []
+    this.layersAndGroups = []
   }
   GROUP.prototype.addLayer = function addLayer(layer) {
-    this.layers.push(layer)
+    this.layersAndGroups.push(layer)
     return layer
   }
   GROUP.prototype.addGroup = function addGroup(group) {
-    this.groups.push(group)
+    this.layersAndGroups.push(group)
     return group
+  }
+  GROUP.prototype.asOpenLayers = function asOpenLayers(source_constructor) {
+    var mapped = []
+    for (var i in this.layersAndGroups) {
+      var layerOrGroup = this.layersAndGroups[i]
+      if (typeof layerOrGroup !== "LAYER" || layerOrGroup.codeIsNumeric()) {
+        var olLayerOrGroup = layerOrGroup.asOpenLayers(source_constructor)
+        mapped.push(olLayerOrGroup)
+      }
+    }
+    var olGroup = new ol.layer.Group({
+      title: this.name,
+      layers: mapped
+    })
+    return olGroup
   }
   return GROUP
 })()
 exports.GROUP = GROUP
 
-var LAYER = function LAYER(code, name, gif, a, b, c, d) {
-  this.code = code
-  this.name = name
-  this.gif = gif
-  this.a = a
-  this.b = b
-  this.c = c
-  this.d = d
-}
+var LAYER = (function () {
+  function LAYER(code, name, gif, a, b, c, visible) {
+    this.element_type = "LAYER"
+    this.code = code
+    this.name = name
+    this.gif = gif
+    this.a = a
+    this.b = b
+    this.c = c
+    this.visible = visible
+  }
+  LAYER.prototype.codeIsNumeric = function codeIsNumeric() {
+    return String(parseInt(this.code, 10)) == this.code
+  }
+  LAYER.prototype.asOpenLayers = function asOpenLayers(source_constructor) {
+    var olSource = source_constructor(this.code)
+    var olLayer = new ol.layer.Image({
+      title: this.name,
+      visible: !!this.visible,
+      source: olSource
+    })
+    return olLayer
+  }
+  return LAYER
+})()
 exports.LAYER = LAYER
